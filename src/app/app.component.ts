b48974195845736d266428e9cc2385213f8f3e19
SicdeX <sicdex@gmail.com>
2023-04-07 17:31:00 +0600

    see desc
    + eslint
    + prettierrc
    + improve env
    + update readme
    ... lint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core"
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'angular-ask-me';
+	title = "angular-ask-me"
+
+	public Test() {
+		return 0
+	}
 }
